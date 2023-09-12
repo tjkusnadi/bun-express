93a3a9ee5076cbd06124d267fc9d9240e58276ca
Tije Kusnadi <tijekusnadi05@gmail.com>
2023-09-12 22:37:52 +0700

    feat(express): init express with bun

---
+++ server.js
@@ -0,0 +1,15 @@
+import "dotenv/config";
+import express from "express";
+
+const app = express();
+const port = Bun.env.port || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}...`);
+});
+
+
